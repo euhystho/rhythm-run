10dcda02b7011905d336a0a8f93a1ce3b47857ad
Eugene Thompson <eugeneyshthom@gmail.com>
2025-03-06 14:28:00 -0500

    Shifted the Flutter Project to main view

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:rhythm_run/main.dart';
+import 'package:rhythm_run/spotify.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

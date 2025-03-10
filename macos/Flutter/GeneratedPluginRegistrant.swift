4aba6cfcd2d0f87030ef8d43c059e2430a5c2dc6
Eugene Thompson <eugeneyshthom@gmail.com>
2025-03-10 16:52:07 -0400

    Added Relevant Packages (health/spotify_sdk/music_kit)

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
+import music_kit_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  MusicKitPlugin.register(with: registry.registrar(forPlugin: "MusicKitPlugin"))
 }

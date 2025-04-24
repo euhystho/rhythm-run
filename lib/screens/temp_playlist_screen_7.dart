217debaae806815368d14b0fe683ca8dd5d5b219
isabelleson <isabelleson5@gmail.com>
2025-04-24 18:55:42 -0400

    added sliders for customization of song suggestions

--- lib/screens/temp_playlist_screen_7.dart
+++ lib/screens/temp_playlist_screen_7.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:rhythmrun/utils/theme.dart';
+import '../data/services/generate_playlist.dart';
+import '../data/types/song.dart';
 
 class TempPlaylistPage extends StatefulWidget {
   const TempPlaylistPage({super.key});
@@ -11,11 +13,14 @@ class TempPlaylistPage extends StatefulWidget {
 
 class TempPlaylistPageState extends State<TempPlaylistPage> {
   // PLEASE DELETE THE SAMPLES LATER THEY ARE FOR TESTING
-  List<String> songList = [
-    'Rick Roll - 114 BPM',
-    'Other Song - 125 BPM',
-    'Wow!! - 130 BPM'
-  ];
+  Playlist songList = Playlist([
+    Song('Rick Roll', 'Rick Astley'),
+    Song('Other Song', 'bbbb'),
+    Song('Wow!!', 'aaaa')
+  ]);
+
+  double threshold = 20;
+  double limit = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -33,45 +38,94 @@ class TempPlaylistPageState extends State<TempPlaylistPage> {
       body: SafeArea(
         child: Column(
           children: [
-            Expanded(
-              child: ReorderableListView.builder(
-                padding: EdgeInsets.zero,
-                shrinkWrap: true,
-                itemCount: songList.length,
-                onReorder: (oldIndex, newIndex) {
-                  setState(() {
-                    if (oldIndex < newIndex) {
-                      newIndex -= 1;
-                    }
-                    final item = songList.removeAt(oldIndex);
-                    songList.insert(newIndex, item);
-                  });
-                },
-                itemBuilder: (context, index) {
-                  final fileName = songList[index];
-                  return Material(
-                    key: Key('$index'),
-                    color: Colors.transparent,
-                    child: ListTile(
-                      title: Text(
-                        fileName,
-                        style: Theme.of(context).textTheme.bodyLarge,
-                      ),
-                      dense: false,
-                      contentPadding: const EdgeInsetsDirectional.fromSTEB(16, 0, 16, 0),
-                      trailing: ReorderableDragStartListener(
-                        index: index,
-                        child: Icon(
-                          Icons.drag_handle,
-                          color: Theme.of(context).colorScheme.onSurfaceVariant,
+             Expanded(
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surface,
+                  border: Border.all(
+                    color: Theme.of(context).dividerColor,
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: ReorderableListView.builder(
+                  padding: EdgeInsets.zero,
+                  itemCount: songList.tracks.length,
+                  onReorder: (oldIndex, newIndex) {
+                    setState(() {
+                      if (oldIndex < newIndex) {
+                        newIndex -= 1;
+                      }
+                      final item = songList.tracks.removeAt(oldIndex);
+                      songList.tracks.insert(newIndex, item);
+                    });
+                  },
+                  itemBuilder: (context, index) {
+                    final fileName = songList.tracks[index].toString();
+                    return Material(
+                      key: Key('$index'),
+                      color: Colors.transparent,
+                      child: ListTile(
+                        title: Text(
+                          fileName,
+                          style: Theme.of(context).textTheme.bodyLarge,
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        trailing: ReorderableDragStartListener(
+                          index: index,
+                          child: Icon(
+                            Icons.drag_handle,
+                            color: Theme.of(context).colorScheme.onSurfaceVariant,
+                          ),
                         ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
             ),
-            if (songList.isNotEmpty)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Suggested song similarity (least to most similar)',
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+                  ),
+                  Slider(
+                    value: threshold,
+                    min: 20,
+                    max: 100,
+                    inactiveColor: Theme.of(context).colorScheme.onSurface,
+                    onChanged: (double value) {
+                      setState(() {
+                        threshold = value;
+                      });
+                    },
+                  ),
+                  Text(
+                    'Number of suggestions per song (0-20)',
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+                  ),
+                  Slider(
+                    value: limit,
+                    max: 20,
+                    inactiveColor: Theme.of(context).colorScheme.onSurface,
+                    onChanged: (double value) {
+                      setState(() {
+                        limit = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+            if (songList.tracks.isNotEmpty)
               Padding(
                 padding: const EdgeInsets.all(16),
                 child: Container(
@@ -99,7 +153,7 @@ class TempPlaylistPageState extends State<TempPlaylistPage> {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
-                              '${songList.length} Songs',
+                              '${songList.tracks.length} Songs',
                               style: Theme.of(context).textTheme.bodyMedium?.copyWith(
                                 fontWeight: FontWeight.w500,
                               ),
@@ -140,6 +194,27 @@ class TempPlaylistPageState extends State<TempPlaylistPage> {
           ],
         ),
       ),
+    );
+  }
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //Toggle the Debug Banner on the side :)
+      debugShowCheckedModeBanner: false,
+      title: 'RhythmRun',
+      theme: RhythmRunTheme.lightTheme,
+      darkTheme: RhythmRunTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      home: const TempPlaylistPage(),
     );
   }
 }

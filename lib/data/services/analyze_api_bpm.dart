6fe5681f3730ae272c065fc1ddcafef5d0f8d6ae
nanodu2604 <khoilucas@gmail.com>
2025-05-06 16:50:50 -0400

    reconstruct file

--- lib/data/services/analyze_api_bpm.dart
+++ lib/data/services/analyze_api_bpm.dart
@@ -1,30 +1,54 @@
-import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';
+import 'dart:io';
+import 'dart:convert';
+import '../types/song.dart';
 
-Future<Database> initDatabase() async {
-  // Get the path to the database
-  final databasePath = await getDatabasesPath();
-  final path = join(databasePath, '../songs.db');
+///TODO: PULL THE PYTHON CALL
+ ////make the python execution
+ ///
+Future <Map<String,dynamic>> pythonExec(Map<String,dynamic> idata) async{
+  final pythonProc= await Process.start('python3'
+                                        , ["/python/get_bpm_copy.py"],
+                                        runInShell: true);
+  pythonProc.stdin.writeln(jsonDecode(idata));
+  await pythonProc.stdin.close();
+
+  final stdout=await pythonProc.stdout.transform(utf8.decoder).join();
+  final stderr=await pythonProc.stderr.transform(utf8.decoder).join();
   
-  // Open the database
-  return await openDatabase(
-    path,
-    version: 1,
-    onCreate: (db, version) async {
-      // Create tables here
-      await db.execute('''
-        CREATE TABLE users(
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          name TEXT,
-          email TEXT
-        )
-      ''');
-    },
-  );
+  try{
+    if (stderr.isNotEmpty){
+      throw "Error: $stderr";
+    }
+    return jsonDecode(stdout);
+  }
+  catch(e){
+    print("Connect fail due to e: $e");
+  }
 }
-
-// Usage
-void main() async {
-  final db = await initDatabase();
-  // Now you can use db to perform queries
+Future<Map<String, dynamic>> playlistToMap(Playlist playlist) async {
+  var data = {
+    "action": "Request",
+    "tracks": [],
+    "artists": []
+  };
+  
+  for (final track in playlist.tracks) {
+    final trackName = track.name;
+    final artist = track.artist;
+    
+    data["tracks"]?.add(trackName);
+    data["artists"]?.add(artist);
+  }
+  
+  return data;
+}
+Future<Playlist>MapToAnalyzePlaylist(Map<String,dynamic> m) async{
+  final n=m["tracks"].length;
+  List<AnalyzedSong> list;
+  for(int i=0;i<n;i++){
+    //put the place holder 30s song 
+    AnalyzedSong a=AnalyzedSong(m["track"][i],m["artists"][i] , 30,m["bpms"][i]);
+    list.add(a);
+  }
+  return Playlist(list);
 }
